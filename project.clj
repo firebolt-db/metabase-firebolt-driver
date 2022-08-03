fef7bdc2f8dff0c6cd9652430f8e3530dab6f5ed
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2022-08-03 06:52:16 +0000

    chore: Update JDBC version (#20)

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject metabase/firebolt-driver "1.0.2"
+(defproject metabase/firebolt-driver "1.0.3"
   :min-lein-version "2.5.0"
 
   :dependencies
-  [[com.firebolt/firebolt-jdbc "1.25"]]
+  [[com.firebolt/firebolt-jdbc "1.28"]]
 
   :repositories [["snapshots" {:sign-releases false
                                :url "https://repo.repsy.io/mvn/firebolt/maven-snapshots"
