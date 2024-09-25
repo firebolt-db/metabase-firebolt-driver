16fb88e8bec3e0d0704465ba520344a6a94c948b
Stepan Burlakov <stepansergeevitch@gmail.com>
2024-09-25 15:43:05 +0300

    report fks as not supported

--- src/metabase/driver/firebolt.clj
+++ src/metabase/driver/firebolt.clj
@@ -260,7 +260,7 @@
 
 (defmethod driver/database-supports? [:firebolt :percentile-aggregations]  [_ _ _] true)
 
-(defmethod driver/database-supports? [:firebolt :foreign-keys]  [_ _ _] true)
+(defmethod driver/database-supports? [:firebolt :foreign-keys]  [_ _ _] false)
 
 (defmethod driver/database-supports? [:firebolt :binning]  [_ _ _] true)
 
