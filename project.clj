371cf15aca8bc7157239401d3ed4b629dc16cd63
Stepan Burlakov <stepansergeevitch@gmail.com>
2024-10-24 13:03:05 +0300

    cleanup project.clj for now since we don't deploy

--- project.clj
+++ project.clj
@@ -29,17 +29,7 @@
   :dependencies
   [[io.firebolt/firebolt-jdbc "3.1.0"]]
 
-  :repositories [["releases" {:sign-releases true
-                               :url "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                               :username :env/MAVEN_REPO_USERNAME
-                               :password :env/MAVEN_REPO_PASSWORD}]
-                 ["snapshots" {:sign-releases true
-                              :url "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                              :username :env/MAVEN_REPO_USERNAME
-                              :password :env/MAVEN_REPO_PASSWORD}]
-                 ["project" "file:repo"]]
-
-  :signing {:gpg-key ~(System/getenv "SIGN_KEY_ID")}
+  :repositories [["project" "file:repo"]]
 
   :plugins [[lein-pprint "1.3.2"]
             [lein-shell "0.5.0"]]
@@ -51,39 +41,7 @@
                                        wget -nv https://downloads.metabase.com/\\$METABASE_VERSION/metabase.jar -O \\$TMP_DIR/metabase.jar && \\
                                        mkdir -p repo && \\
                                        mvn deploy:deploy-file -Durl=file:repo -DgroupId=com.firebolt -DartifactId=metabase-core -Dversion=1.40 -Dpackaging=jar -Dfile=\\$TMP_DIR/metabase.jar"]]
-            "sign" ["shell" "gpg" "-b" "-a" "--yes" "-u" ~(System/getenv "SIGN_KEY_ID")]
-            ;; lein just doesn't know how to deploy uberjar, so we have to do everything manually
-            "deploy-uberjar" ["do"
-                              "uberjar,"
-                              "pom,"
-                              "sign" ~(str uberjar-file ",")
-                              "sign" "pom.xml,"
-                              ;; There is some internal magic that renames pom.xml.asc to xml.asc during deploy,
-                              ;; so we have to hack it
-                              "shell" "mv" "pom.xml.asc" "pom.pom.asc,"
-                              "deploy" "releases" "io.firebolt/firebolt.metabase-driver" ~(str version)
-                              ~(str uberjar-file) "pom.xml"
-                              ~(str uberjar-file ".asc") "pom.pom.asc"
-                              ]
             }
-
-  :pom-plugins [[org.apache.maven.plugins/maven-source-plugin "3.2.1"
-                 ;; this section is optional, values have the same syntax as pom-addition
-                 {:executions ([:execution [:id "attach-sources"]
-                                [:goals ([:goal "uberjar"])]
-                                [:phase "deploy"]])}]
-                [org.apache.maven.plugins/maven-javadoc-plugin "3.3.1"
-                 {:executions ([:execution [:id "attach-javadocs"]
-                                [:goals ([:goal "uberjar"])]
-                                [:phase "deploy"]])}]]
-
-  ;; Fix issue with Azure <-> Maven communication
-  ;; issue happens when we're trying to deploy from GitHub Actions
-  ;; we would receive a SockedClosed exception
-  :jvm-opts ["-Dmaven.wagon.http.pool=false"
-             "-Dmaven.wagon.http.connectionManager.ttlSeconds=60000"
-             "-Dmaven.wagon.http.timeout=120000000"
-             "-Dmaven.wagon.http.retryHandler.count=20"]
 
   :profiles
   {:provided
