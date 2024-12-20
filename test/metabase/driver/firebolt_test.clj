0d997c1c38b25981ede1b672584e77d7321eec78
Stepan Burlakov <stepan.burlakov@firebolt.io>
2024-12-20 15:27:21 +0200

    add tests

--- test/metabase/driver/firebolt_test.clj
+++ test/metabase/driver/firebolt_test.clj
@@ -243,3 +243,56 @@
                                       (is (= [{:name "example_view"}]
                                              (filter (has-value :name "example_view") (map (partial into {})
                                                                                            (t2/select [Table :name] :db_id (u/the-id database))))))))))))
+
+; test that this method works
+; (defn array-to-string [os]
+;    (let [convert-element (fn [element]
+;    (do (log/info "Class: " (.getName (.getClass element)) "is array" (.isArray (class element)))
+;                            (cond
+;                              (.isArray (class element))
+;                              (array-to-string (into [] element)) ; Handle nested arrays
+;                              (instance? java.sql.Timestamp element)
+;                              (.format (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss") element)
+;                              (instance? java.lang.String element)
+;                              (str "'" element "'")
+;                              :else (.toString element))))]
+;      (str "[" (clojure.string/join "," (map convert-element os)) "]")))
+;  
+;  ; Handle array data type for Firebolt
+;  (defmethod metabase.driver.sql-jdbc.execute/read-column-thunk [:firebolt Types/ARRAY]
+;     [_ ^ResultSet rs _ ^Integer i]
+;     (fn []
+;       (let [os (.getArray rs i)]
+;         (if (nil? os)
+;           nil  ; Return nil if the array is null
+;           (array-to-string (into [] (.getArray os)))))))
+(deftest array-to-string-test
+    (mt/test-driver :firebolt
+        (testing "array-to-string"
+            (let [expected "[1,2,3,4,5]"
+                actual   (sql-jdbc.execute/read-column-thunk :firebolt Types/ARRAY
+                                                             (proxy [ResultSet] []
+                                                                 (getArray [i] (proxy [Array] []
+                                                                                 (getArray [] [1 2 3 4 5]))))
+                                                             1)]
+            (is (= expected actual))))
+        (testing "array-to-string with nested arrays"
+            (let [expected "[[1,2,3],[4,5,6],[7,8,9]]"
+                actual   (sql-jdbc.execute/read-column-thunk :firebolt Types/ARRAY
+                                                             (proxy [ResultSet] []
+                                                                 (getArray [i] (proxy [Array] []
+                                                                                 (getArray [] [[1 2 3] [4 5 6] [7 8 9]])))
+                                                                 (getArray [] [[1 2 3] [4 5 6] [7 8 9]]))
+                                                             1)]
+            (is (= expected actual))))
+       (testing "array-to-string with datetime"
+            (let [expected "[\"2021-06-06 12:12:12\",\"2021-06-06 12:12:12\"]"
+                actual   (sql-jdbc.execute/read-column-thunk :firebolt Types/ARRAY
+                                                             (proxy [ResultSet] []
+                                                                 (getArray [i] (proxy [Array] []
+                                                                                 (getArray [] [LocalTime/parse "12:12:12" ZonedDateTime/parse "2021-06-06T12:12:12Z"])))
+                                                                 (getArray [] [LocalTime/parse "12:12:12" ZonedDateTime/parse "2021-06-06T12:12:12Z"]))
+                                                             1)]
+            (is (= expected actual))))
+    )
+)
